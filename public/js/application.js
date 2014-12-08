b7cacc5d14ce57aa90e27b6cc50790ad52606e03
Aaron Piotrowski <a.paul.piotrowski@gmail.com>
2014-12-08 11:15:46 -0600

    more cleaning

--- public/js/application.js
+++ public/js/application.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
     };
 
 // ---------- RADIAL BACKGROUND ----------
-    // background-image: radial-gradient(farthest-corner at 600px 45px , #FF0000 0%, #0000FF 100%);
+// background-image: radial-gradient(farthest-corner at 600px 45px , #FF0000 0%, #0000FF 100%);
 
 // clicked = 1
 // top = 27       45
@@ -35,27 +35,25 @@ $(document).ready(function() {
 // top = 27
 // left = 1181
 
-
 // 45, 45
 
+// var position = $(this).position();
+// var top = position["top"]
+// var left = position["left"]
+// console.log(position["top"]);
+// console.log(position["left"]);
 
-    // var position = $(this).position();
-    // var top = position["top"]
-    // var left = position["left"]
-    // console.log(position["top"]);
-    // console.log(position["left"]);
+// $("#tabla").css("background-image",  "radial-gradient(farthest-side, "+top+"px "+left+"px, #FF0000, #0000FF)");
 
-    // $("#tabla").css("background-image",  "radial-gradient(farthest-side, "+top+"px "+left+"px, #FF0000, #0000FF)");
-
-    // if((value >= (computerNumber + 6)) || (value <= (computerNumber - 6))){
-    //   $(this).css("background-color", "linear-gradient(to right, #1e5799 0%,blue 0%, red 100%)");
-    //   clickCount -= 1;
-    //   $('.num_remaining').html(clickCount);
-    // } else if((value <= (computerNumber + 6)) || (value >= (computerNumber - 6))){
-    //   $(this).css("background-color", "linear-gradient(to right, #1e5799 0%,blue 0%, red 100%)");
-    //   clickCount -= 1;
-    //   $('.num_remaining').html(clickCount);
-    // }
+// if((value >= (computerNumber + 6)) || (value <= (computerNumber - 6))){
+//   $(this).css("background-color", "linear-gradient(to right, #1e5799 0%,blue 0%, red 100%)");
+//   clickCount -= 1;
+//   $('.num_remaining').html(clickCount);
+// } else if((value <= (computerNumber + 6)) || (value >= (computerNumber - 6))){
+//   $(this).css("background-color", "linear-gradient(to right, #1e5799 0%,blue 0%, red 100%)");
+//   clickCount -= 1;
+//   $('.num_remaining').html(clickCount);
+// }
 
 // ---------- LINEAR GRADIENT ----------
     if(value < computerNumber){
@@ -69,8 +67,6 @@ $(document).ready(function() {
       } else if(value > computerNumber){
       var num = Math.abs((computerNumber-value));
       console.log(computerNumber);
-      // var position = $(this).position();
-      // console.log(position)
 
       $(this).css("background", "linear-gradient(to left, #1e5799 50%, #c0392b  50%");
       clickCount -= 1;
@@ -81,11 +77,6 @@ $(document).ready(function() {
 
 });
 
-
-
-// Formal grammar: radial-gradient( [[ circle || <length> ] [ at <position> ]? , | [ ellipse || [<length> | <percentage> ]{2}] [ at <position> ]? , | [ [ circle | ellipse ] || <extent-keyword> ] [ at <position> ]? , | at <position> , <color-stop> [ , <color-stop> ]+ ) \------------------------------------------------------------------------------/\--------------------------------/ Definition of the contour, size and position of the ending shape List of color stops where <extent-keyword> = closest-corner | closest-side | farthest-corner | farthest-side and <color-stop> = <color> [ <percentage> | <length> ]? Definition of the ending shape radial-gradient( circle, … ) /* Synonym of radial-gradient( circle farthest-corner, …) */ radial-gradient( ellipse, … ) /* Synonym of radial-gradient( ellipse farthest-corner, …) */ radial-gradient( <extent-keyword>, … ) /* It draws a circle */ radial-gradient( circle radius, … ) /* A centered circle of the given length. It can't be a percentage */ radial-gradient( ellipse x-axis y-axis, … ) /* The two semi-major axis are given, horizontal, then vertical */ Definition of the position of the shape radial-gradient (… at <position>, … ) Definition of the color stops radial-gradient (…, <color-stop>) radial-gradient (…, <color-stop>, <color-stop>)
-
-// background-image: radial-gradient(farthest-corner at 45px 45px , #FF0000 0%, #0000FF 100%);
 
 
 
